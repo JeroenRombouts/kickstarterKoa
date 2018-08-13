d223bfc26e84177c3a7f930081d5f030ded2ab6a
Jeroen Rombouts <jeroen.rombouts@ordina.be>
2018-08-13 22:01:07 +0200

    add koajson

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
+const json = require('koa-json')
 const Koa = require('koa');
 
 const app = new Koa();
+app.use(json());
 const PORT = 1337;
 
 app.use(async(ctx) => {
     ctx.body = {
-        status: 'success',
         message: 'hello, world!'
     };
 });
