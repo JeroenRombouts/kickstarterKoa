cc2a716c10f1fc59a285564552119b6b59d276e1
Jeroen Rombouts <jeroen.rombouts@ordina.be>
2018-08-14 13:57:50 +0200

    add koa-cors

--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
 const json = require('koa-json')
 const Koa = require('koa');
-
+const cors = require('koa-cors');
 const app = new Koa();
+
+var options = {
+    origin: '*'
+};
+
+app.use(cors(options));
 app.use(json());
 const PORT = 1337;
 
 app.use(async(ctx) => {
     ctx.body = {
-        message: 'hello, world!'
+        "products": [
+            { "Name": "Cheese", "Price": 2.50, "Location": "Refrigerated foods" },
+            { "Name": "Crisps", "Price": 3, "Location": "the Snack isle" },
+            { "Name": "Pizza", "Price": 4, "Location": "Refrigerated foods" },
+            { "Name": "Chocolate", "Price": 1.50, "Location": "the Snack isle" },
+            { "Name": "Self-raising flour", "Price": 1.50, "Location": "Home baking" },
+            { "Name": "Ground almonds", "Price": 3, "Location": "Home baking" },
+        ]
     };
 });
 
